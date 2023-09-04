4e5c9ec2faa50f09d49a1d912875f5d38b89c8f3
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-04 16:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
-function volume_sphere() {
-    //Write your code here
-	let rad=document.getElementById("radius");
-	let volume=document.getElementById("volume");
-	let radius=rad.value;
-	let vol=(4/3)*3.142*(radius*radius*radius);
-	vol = vol.toFixed(4);
-	//let v=54;
-	 volume.value=vol;
-	return false;
-	
-  
-} 
- let button=document.getElementById("submit");
- button.addEventListener("click",volume_sphere);
-
+function volume_sphere()
+ {
+  var volume;
+  var radius = document.getElementById('radius').value;
+  radius = Math.abs(radius);
+  volume = (4/3) * Math.PI * Math.pow(radius, 3);
+  volume = volume.toFixed(4);
+  document.getElementById('volume').value = volume;
+  return false;
+ } 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
