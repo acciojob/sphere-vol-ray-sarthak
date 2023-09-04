22925699497f5f7f9890305cf6391703bc9529ad
ray-sarthak <117006899+ray-sarthak@users.noreply.github.com>
2023-09-04 16:02:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ function volume_sphere() {
 	let rad=document.getElementById("radius");
 	let volume=document.getElementById("volume");
 	let radius=rad.value;
-	const vol=(4/3)*3.142*(radius*radius*radius);
+	let vol=(4/3)*3.142*(radius*radius*radius);
+	vol = vol.toFixed(4);
 	//let v=54;
 	 volume.value=vol;
+	return false;
 	
   
 } 
  let button=document.getElementById("submit");
  button.addEventListener("click",volume_sphere);
 
-//window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
